23ae27eff8a6337f10de48d646ed8a4e2f285739
user <pva2687@gmail.com>
2017-10-03 16:03:28 +0300

    fix task

--- task4/script.py
+++ task4/script.py
@@ -50,79 +50,12 @@ class MainCalculate:
 # calculations for drawing
 def calculate(win, my_turtle, f_type, iter_num, points_count):
 	cal = MainCalculate(my_turtle)
-	win.bgcolor("#c5f079")
-	for i in range(iter_num):
-		amp = cal.amp  # scaling factor
-		# generate coefficients for affine transformation
-		a = random.random()
-		d = random.uniform(a * a, 1.0)
-		b = random.random()
-		e = random.uniform(b * b, 1.0)
-		c = random.uniform(-1.5, 1.5)
-		f = random.uniform(-1.5, 1.5)
-		# generate random color
-		rr = random.randint(0, 150)
-		gg = random.randint(0, 150)
-		bb = random.randint(0, 150)
-		xmin = -1.25
-		xmax = 1.25
-		ymin = -0.35
-		ymax = 0.35
 
-
-		# run calculations and drawing
-		for j in range(points_count):
-			# generate x & y for nonlinear transformation
-			newx = random.uniform(xmin, xmax)
-			newy = random.uniform(ymin, ymax)
-			x = a * newx + b * newy + c
-			y = d * newx + e * newy + f
-			# apply random color
-			my_turtle.pencolor(rr, gg, bb)
-			if f_type == "sinus":
-				cal.sinus_cal(x, y)
-			if f_type == "heart":
-				cal.heart_cal(x, y)
-			if f_type == "spherical":
-				cal.spherical_cal(x, y)
-			if f_type == "polar":
-				x1 = (math.atan(y/x)/math.pi) * amp
-				y1 = (math.sqrt(x*x + y*y) - 1) * amp
-				draw(my_turtle, x1, y1)
-			if f_type == "disk":
-				x1 = ((1/math.pi) * math.atan(y/x) * math.sin(math.pi * math.sqrt(x*x + y*y))) * amp
-				y1 = ((1/math.pi) * math.atan(y/x) * math.cos(math.pi * math.sqrt(x*x + y*y))) * amp
-				draw(my_turtle, x1, y1)
 
 def main():
 	# parse arguments and set values for vars
 	args = parser.parse_args()
-	f_type = args.type  # set type of nonlinear transformation
 
-	# set number of iterations
-	if args.iter_num is not None:
-		iter_num = args.iter_num
-	else:
-		iter_num = 7
-
-	# set points count for 1 iteration
-	if args.points_count is not None:
-		points_count = args.points_count
-	else:
-		points_count = 150
-
-	# check type of nonlinear transformation
-	if f_type in ("sinus", "heart", "spherical", "polar", "disk"):
-		win = turtle.Screen()
-		my_turtle = turtle.Turtle()
-		my_turtle.screen.colormode(255)
-		my_turtle.speed(0)
-
-		# call function for draw fractal flame
-		calculate(win, my_turtle, f_type, iter_num, points_count)
-		win.exitonclick()
-	else:
-		print("Please set correct type of nonlinear transformation\nFor get help run this scripts with parameter -h")
 
 
 if __name__ == '__main__':
