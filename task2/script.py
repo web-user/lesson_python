26025512f7a2164715e07a53d7be1b22d570e0b3
user <pva2687@gmail.com>
2017-10-04 08:40:32 +0300

    fix task2

--- task2/script.py
+++ task2/script.py
@@ -1,49 +1,31 @@
 """Turtle Graphics"""
-
-import re
 from turtle import *
 
+RADIUS= 200
+radius = 200
 turtle = Turtle()
 
-def search(st):
-	document_text = st
-	text_string = document_text.lower()
-	match_pattern = re.findall(  r'\w*', text_string)
-	frequency = { }
-	word_ret = {}
-	for word in match_pattern:
-		count = frequency.get(word,0)
-		frequency[word] = count + 1
 
-	frequency_list = frequency.keys()
+def get_words_percentage(text):
+	words_count = {}
+	words = text.split(' ')
+	for word in words:
+		words_count[word] = words_count.get(word, 0) + 1 
+	return words_count
 
-	for words in frequency_list:
-		word_ret[words] = frequency[words] 
-	
-	return word_ret
+#words_count.values()
+#words_count.keys()
 
+res = get_words_percentage('hello hell hell  12 web')
+#res = {'hello': 1, 'hell': 2, '12': 1, 'web':1} 
 
-res = search('hello hell hell  12 web')
-
-
-per = []
-
-for item in res:
-	if item != '':
-		per.append(res[item])
+per = [word_count for word, word_count in res.items() if word ]
 
 sum_str = sum(per)
 
-def cal_list( x, y ):
-	new_l = []
-	for i in x:
-		my_r = i / y * 1
-		new_l.append(my_r)
-	return new_l
+percentages = [word_count / sum_str for word_count in per]
 
-percentages = cal_list(per, sum_str)
-
-radius = 200
+#percentages = cal_list(per, sum_str)
 
 color_num = ['blue','red','green','white','yellow', 'violet','orange']
 
@@ -61,8 +43,8 @@ right(90)
 def segment(percentages, radius):
 	rollingPercent = 0
 	for percent in percentages:
-		segment = percent * 360
-		rollingPercent += segment
+		rollingPercent  += percent * 360
+		
 		color('yellow')
 		setheading(rollingPercent)
 		# color(clr[int_cl])
--- task4/script.py
+++ task4/script.py
@@ -11,8 +11,6 @@ parser.add_argument('-i', '--iter_num', type=int, help='Number of iterations. De
 parser.add_argument('-p', '--points_count', type=int,
 					help="Number of points for 1 iteration. More points - best picture. Default value is: 150")
 
-
-
 class MainCalculate:
 
 	amp = 120  # scaling factor
@@ -27,32 +25,49 @@ class MainCalculate:
 		self.turtle.pendown()
 		self.turtle.fd(5)
 
-		
-
-	def sinus_cal(self, x, y):
+	def sinus(self, x, y):
 		self.x = math.sin(x) * self.amp
 		self.y = math.sin(y) * self.amp
 		self.draw_m(self.x, self.y)
+		# print("This func sin")
 
-	def heart_cal(self, x, y):
+	def heart(self, x, y):
 		self.x = (math.sqrt(x * x + y * y) * math.sin(math.sqrt(x * x + y * y) * math.atan(y/x))) * self.amp
 		self.y = (-math.sqrt(x * x + y * y) * math.cos(math.sqrt(x * x + y * y) * math.atan(y/x))) * self.amp
 		self.draw_m(self.x, self.y)
+		# print("This func heart")
 
-	def spherical_cal(self, x, y):
+	def spherical(self, x, y):
 		self.x =  (x / (x*x + y*y)) * self.amp
 		self.y = (y / (x*x + y*y)) * self.amp
 		self.draw_m(self.x, self.y)
 
+	def polar(self, x, y):
+		self.x = (math.atan(y/x)/math.pi) * self.amp
+		self.y = (math.sqrt(x*x + y*y) - 1) * self.amp
+		self.draw_m(self.x, self.y)
 
+	def disk(self, x, y):
+		self.x = ((1/math.pi) * math.atan(y/x) * math.sin(math.pi * math.sqrt(x*x + y*y))) * self.amp
+		self.y = ((1/math.pi) * math.atan(y/x) * math.cos(math.pi * math.sqrt(x*x + y*y))) * self.amp
+		self.draw_m(self.x, self.y)
 
+	def all_cal(self, f_type, x, y):
+		param = [self.sinus, self.heart]
+		getattr(self, f_type)(x, y)
 
+XMIN= 1.777
+XMAX = 1.777
+YMIN = -1
+YMAX = 1
 # calculations for drawing
 def calculate(win, my_turtle, f_type, iter_num, points_count):
 	cal = MainCalculate(my_turtle)
 
 
+
 def main():
+
 	# parse arguments and set values for vars
 	args = parser.parse_args()
 
