2c5f903b08cddc153dd59b27f324e1c2cd1e0646
user <pva2687@gmail.com>
2017-12-14 10:50:23 +0200

    2048 settings

--- 2048_python.py
+++ 2048_python.py
@@ -4,6 +4,7 @@ import termios
 from random import randint
 from game_settings import *
 
+
 GRID_LEN = 4
 
 class GetCh:
@@ -22,69 +23,42 @@ class Game(GameSettings):
     def __init__(self):
         super().__init__()
         self.init_matrix()
-        self.update_grid_cells()
-        self.new_number = 0
-
-    def gen(self):
-        return randint(0, GRID_LEN - 1)
+        self.new_number = []
 
     def init_matrix(self):
         self.matrix = self.new_game(GRID_LEN)
-
         self.matrix = self.add_two(self.matrix)
         self.matrix = self.add_two(self.matrix)
 
-        # print(self.matrix)
-
-    def update_grid_cells(self):
-        for i in range(GRID_LEN):
-            for j in range(GRID_LEN):
-                new_number = self.matrix[i][j]
-
+    def update_grid_cells(self, matrix_list):
+        self.matrix = matrix_list
+        self.matrix = self.add_two(self.matrix)
+        return self.matrix
 
 def get():
     inkey = GetCh()
 
-    # start_game = Game()
-
     gamegrid = Game()
-
-    print(gamegrid.new_number)
-
-    # board = gamegrid.generate_next()
-
-    # for row in gamegrid.matrix:
-        # print("".join(str(row)))
-
-
-    # board = []
-
-    # for x in range(4):
-    #     board.append(["X"] * 4)
-
-    # start_game.set_random_number(board)
 
     while(1):
         key = inkey()
         if key != '' :break
     if key == '\x1b[A':
-        gamegrid.iteration_list(gamegrid.up(gamegrid.matrix))
+        gamegrid.iteration_list(gamegrid.update_grid_cells(gamegrid.up(gamegrid.matrix)))
         print("up")
     elif key == '\x1b[B':
-        gamegrid.iteration_list(gamegrid.down(gamegrid.matrix))
+        gamegrid.iteration_list(gamegrid.update_grid_cells(gamegrid.down(gamegrid.matrix)))
         print("down")
     elif key == '\x1b[C':
-        gamegrid.iteration_list(gamegrid.right(gamegrid.matrix))
+        gamegrid.iteration_list(gamegrid.update_grid_cells(gamegrid.right(gamegrid.matrix)))
     elif key == '\x1b[D':
         # start_game.move_set(board, 'left')
-        gamegrid.iteration_list(gamegrid.left(gamegrid.matrix))
+        gamegrid.iteration_list(gamegrid.update_grid_cells(gamegrid.left(gamegrid.matrix)))
         # print("left")
     else:
         print("not an arrow key!")
         exit()
 
-    # for row in board:
-    #     print(" ".join(row))
 
 def main():
     for i in range(0,20):
